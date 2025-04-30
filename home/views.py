a14c2b1745094eef1214911a8da61e494fe04492
JoaoInacioFussiegerIzaias <joaoizaias10@gmail.com>
2025-04-30 14:14:47 -0300

    Home/Front

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib import messages
+
+def index(request):
+    try:
+        if request.method == "GET":        
+            return render(request, "home.html")
+        messages.error(request, _("Método Http não permitido!"))
+    except Exception as e:
+        messages.error(request, str(e))
