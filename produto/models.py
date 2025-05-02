992f459bdbe317fa977b76390d66034a8c3f41a7
JoaoInacioFussiegerIzaias <joaoizaias10@gmail.com>
2025-05-02 10:10:38 -0300

    Categoria + Produto

--- produto/models.py
+++ produto/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from categoria.models import Categoria
+from datetime import datetime
 
 class Produto(models.Model):
     cod_barras = models.IntegerField(null=False)
     nome = models.CharField(max_length=50)
     quantidade = models.IntegerField(default=0)
-    preco = models.DecimalField(null=False)
-
+    preco = models.DecimalField(null=False, max_digits=50,decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    date = models.DateTimeField(default=datetime.now, blank=True)
