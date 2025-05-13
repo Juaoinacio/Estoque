a8bf6343beac63fded8c531326770fa9d34be9ca
Seu Nome <seu@email.com>
2025-05-13 20:23:55 -0300

    View min, crit, zerado

--- home/views.py
+++ home/views.py
@@ -6,5 +6,15 @@ from django.urls import reverse
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 
+
 def index(request):
-    return render(request, "home.html")
+    minimo = Produto.objects.filter(quantidade__gte=100, quantidade__lte=200)
+
+    valorMinimo = len(minimo)
+    critico = Produto.objects.filter(quantidade__gte=1, quantidade__lte=100)
+
+    valorCritico = len(critico)
+    zerado = Produto.objects.filter(quantidade__in=[0])
+
+    valorZerado= len(zerado)
+    return render(request, "home.html", {'minimo': valorMinimo, 'critico': valorCritico,'zerado': valorZerado })
