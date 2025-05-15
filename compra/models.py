3cc7cd83f043bd3120d5677404c2efbea20ca5c4
Juaoinacio <joaoinacio10@gmail.com>
2025-05-15 11:48:49 -0300

    Model Compra

--- compra/models.py
+++ compra/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from datetime import datetime
+from tipoDePagamento.models import TipoDePagamento 
+from fornecedor.models import Fornecedor 
 
-# Create your models here.
+class Compra(models.Model):
+    nome = models.CharField(max_length=50)
+    notaFiscal = models.CharField(max_length=60, validators=[RegexValidator(regex='^\d+$', message='O código de barras deve conter apenas números.')], null=False)
+    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.PROTECT)
+    tipoDePagamento = models.ForeignKey(TipoDePagamento, on_delete=models.PROTECT)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+     
+    class Meta:
+        db_table = "Compra"
+
+    def __str__(self):
+        return f"Nome: {self.nome} "
