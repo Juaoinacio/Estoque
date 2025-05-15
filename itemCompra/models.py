957856685bbb05c3e095a88b22da9c9431a32b34
Juaoinacio <joaoinacio10@gmail.com>
2025-05-15 13:19:30 -0300

    Model ItemCompra

--- itemCompra/models.py
+++ itemCompra/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from produto.models import Produto
 
-# Create your models here.
+class ItemCompra(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.PROTECT)
+    quantidade = models.PositiveIntegerField()
+    precoUnitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Meta:
+    db_table = "ItemCompra" 
+
+def __str__(self):
+    return f"Nome: {self.nome} "
