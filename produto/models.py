c2b812757d9eedc738a9c5863286d862421a330c
Juaoinacio <joaoinacio10@gmail.com>
2025-05-15 11:22:40 -0300

    Mudança de planos de sistema de estoques

--- produto/models.py
+++ produto/models.py
@@ -7,7 +7,8 @@ class Produto(models.Model):
     cod_barras = models.CharField(max_length=20, validators=[RegexValidator(regex='^\d+$', message='O código de barras deve conter apenas números.')], null=False)
     nome = models.CharField(max_length=50)
     quantidade = models.IntegerField(default=0)
-    preco = models.DecimalField(null=False, max_digits=50,decimal_places=2)
+    valorPago = models.DecimalField(null=False, max_digits=50,decimal_places=2)
+    valorVenda = models.DecimalField(null=False, max_digits=50,decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
     date = models.DateTimeField(default=datetime.now, blank=True)
 
