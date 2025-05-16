d21964d7466500d1b076017e700998593d5e86cc
Juaoinacio <joaoinacio10@gmail.com>
2025-05-16 09:10:02 -0300

    Model fornecedor

--- compra/views.py
+++ compra/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
+from .models import ItemCompra, Compra
 def index(request):
     try:
         if  request.method == "GET":
-               
+            context = {'compra': Compra.objects.all(), 'item': ItemCompra.objects.all()}
 
-            return render(request, "comprar.html")
+            return render(request, "comprar.html", context)
         else:
             pass
     except:
